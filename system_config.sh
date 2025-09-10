394610de8f100096faa5dbacd93c1b8217efb199
marcelsachs <sachsmarcel@proton.me>
2025-09-11 01:03:01 +0200

    Update system_config.sh

--- system_config.sh
+++ system_config.sh
@@ -13,7 +13,8 @@ for loc in "${LOCALES[@]}"; do
     sed -i 's/^# *$loc/$loc/' /etc/locale.gen
 done
 locale-gen
-echo 'LANG=$LANG' > /etc/locale.conf
-echo 'KEYMAP=$KEYMAP' > /etc/vconsole.conf
-echo '$HOSTNAME' > /etc/hostname
-echo -e '127.0.0.1 localhost\n::1 localhost\n127.0.1.1 $HOSTNAME.localdomain $HOSTNAME' > /etc/hosts
+echo "root:$ROOT_PASSWORD" | chpasswd
+echo "LANG=$LANG" > /etc/locale.conf
+echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+echo "$HOSTNAME" > /etc/hostname
+echo -e "127.0.0.1 localhost\n::1 localhost\n127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" > /etc/hosts
