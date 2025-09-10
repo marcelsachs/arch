a855597588f62dccc2b2cf8ba9dcfe2bcac30e61
marcelsachs <sachsmarcel@proton.me>
2025-09-11 01:02:20 +0200

    Update user.sh

--- user.sh
+++ user.sh
@@ -6,6 +6,6 @@ set -euo pipefail
 source config.sh
 
 echo "-> Creating user $USER_NAME..."
-useradd -m -d '$HOME_DIR' -s /bin/bash "$USER_NAME"
-echo '$USER_NAME:$USER_PASSWORD' | chpasswd
-echo '$USER_NAME ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/$USER_NAME
+useradd -m -d "$HOME_DIR" -s /bin/bash "$USER_NAME"
+echo "$USER_NAME:$USER_PASSWORD" | chpasswd
+echo "$USER_NAME ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$USER_NAME
