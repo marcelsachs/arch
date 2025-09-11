17f488ea91d3ad028ce56cac54d9fa51559af8a4
marcelsachs <sachsmarcel@proton.me>
2025-09-11 16:05:09 +0200

    yes

--- clone_repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# clone_repo.sh - Clone GitHub repo (optional)
-
-set -euo pipefail
-
-source config.sh
-
-echo "-> Cloning user repository..."
-su - $USER_NAME -c "git clone $GITHUB_REPO"
